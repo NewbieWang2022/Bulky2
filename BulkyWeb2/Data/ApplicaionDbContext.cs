b42c74863af144b1c46354edcf0e9f4e1a2f3f67
yutan <yutan@TABLET-4OEG3OS5>
2024-04-23 15:59:06 +0800

    加入專案檔案。

---
+++ BulkyWeb2/Data/ApplicaionDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb2.Data
+{
+    public class ApplicaionDbContext : DbContext
+    {
+        public ApplicaionDbContext(DbContextOptions<ApplicaionDbContext> options) : base(options) { 
+        
+        }
+    }
+}
