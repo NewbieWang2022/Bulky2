b4d0de9ccbe17fac7e6ad08b465431d59cc008be
郁棠 王 <yutang2023@outlook.com>
2024-06-04 08:11:10 +0800

    no action when clicking on Create category list

--- BulkyWeb2/Controllers/CategoryController.cs
+++ BulkyWeb2/Controllers/CategoryController.cs
@@ -1,13 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using BulkyWeb2.Data;
+﻿using BulkyWeb2.Data;
+using Microsoft.AspNetCore.Mvc;
 using BulkyWeb2.Models;
 
 namespace BulkyWeb2.Controllers
 {
-    public class CategoryController : Controller 
+    public class CategoryController : Controller
     {
-        private readonly ApplicationDbContext _db;
-        public CategoryController(ApplicationDbContext db)
+        private readonly ApplicaionDbContext _db;
+        public CategoryController(ApplicaionDbContext db)
         {
             _db = db;
         }
@@ -15,6 +15,10 @@ namespace BulkyWeb2.Controllers
         public IActionResult Index()
         {
             List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+        public IActionResult Create()
+        {
             return View();
         }
     }
